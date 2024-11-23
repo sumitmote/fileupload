e57cde9ed9898250c96666b9db709f346ec1f850
Sumit\Sumatoli <sumitmote19@gmail.com>
2024-11-23 11:36:40 +0530

    first_commit

---
+++ src/main/java/com/javatechie/controller/FileController.java
@@ -0,0 +1,43 @@
+package com.javatechie.controller;
+
+import com.javatechie.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/image")
+public class FileController {
+
+
+
+    @Autowired
+    private StorageService service;
+
+    @PostMapping
+    public ResponseEntity<?> uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
+        String uploadImage = service.uploadImage(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        String message="HI WELCOME TO FILE UPLOAD TEST APP";
+        return ResponseEntity.status(HttpStatus.OK).body(message);
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<?> downloadImage(@PathVariable String fileName){
+        byte[] imageData=service.downloadImage(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageData);
+
+    }
+}
